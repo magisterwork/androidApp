30771dd1188650434b64b2ec768304b41725efc6
kalyashov <kalyashov@yandex.ru>
2016-11-13 16:18:40 +0300

    Доработка верстки новостной ленты, часть 1.

--- app/src/main/java/com/app/eventsapp/MainActivity.java
+++ app/src/main/java/com/app/eventsapp/MainActivity.java
@@ -2,12 +2,8 @@ package com.app.eventsapp;
 
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.app.eventsapp.core.app.EventsApp;
 import com.app.eventsapp.core.base.BaseActivity;
@@ -52,37 +48,6 @@ public class MainActivity extends BaseActivity implements MainActivityView, HasC
                     .replace(R.id.fragment_container, postLineFragment)
                     .commit();
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
