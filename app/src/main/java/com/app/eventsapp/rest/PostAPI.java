e32abf39b6ce34778f9f453a0f8cda5a64effff5
kalyashov <kalyashov@yandex.ru>
2016-11-20 00:19:16 +0300

    В PostAPI добавлены новые методы для получения событий

--- app/src/main/java/com/app/eventsapp/rest/PostAPI.java
+++ app/src/main/java/com/app/eventsapp/rest/PostAPI.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by Grigory Kalyashov on 04.11.2016.
@@ -19,4 +20,20 @@ public interface PostAPI
      */
     @GET("events/api/list")
     Call<List<Post>> getPosts();
+
+    /**
+     * Получить список событий
+     * @param offset сдвиг
+     * @param count количество событий
+     * @return список
+     */
+    @GET("events/api/list?offset={offset}&count={count}")
+    Call<List<Post>> getPosts(@Path("offset") int offset, @Path("count") int count);
+
+    /**
+     * @param id - id события
+     * @return пост
+     */
+    @GET("events/api/get/{id}")
+    Call<Post> getPost(@Path("id") Long id);
 }
