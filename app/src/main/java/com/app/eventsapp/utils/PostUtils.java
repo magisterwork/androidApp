c826700a21d99c28d639af0fba2051b4f4e82c8a
kalyashov <kalyashov@yandex.ru>
2016-11-20 23:29:10 +0300

    Докоммит

---
+++ app/src/main/java/com/app/eventsapp/utils/PostUtils.java
@@ -0,0 +1,10 @@
+package com.app.eventsapp.utils;
+
+/**
+ * Created by Grigory Kalyashov on 19.11.2016.
+ */
+public class PostUtils
+{
+    // ключ для передачи id поста через Bundle
+    public static final String postIdBundleKey = "post_id";
+}
