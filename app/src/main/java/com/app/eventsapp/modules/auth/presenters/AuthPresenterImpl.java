c60a2e0b7c110ff32f5efd39d6b6f7a1c325d63a
kalyashov <kalyashov@yandex.ru>
2017-02-19 17:42:59 +0300

    Отправка google токена на сервер

--- app/src/main/java/com/app/eventsapp/modules/auth/presenters/AuthPresenterImpl.java
+++ app/src/main/java/com/app/eventsapp/modules/auth/presenters/AuthPresenterImpl.java
@@ -1,11 +1,22 @@
 package com.app.eventsapp.modules.auth.presenters;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.app.eventsapp.modules.auth.models.User;
+import com.app.eventsapp.modules.auth.rest.AuthService;
 import com.app.eventsapp.modules.auth.session.UserSessionManager;
 import com.app.eventsapp.modules.auth.views.AuthFragmentView;
+import com.app.eventsapp.rest.request.RequestListener;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.inject.Inject;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * Created by Grigory Kalyashov on 19.02.2017.
@@ -14,6 +25,10 @@ public class AuthPresenterImpl implements AuthPresenter
 {
     private AuthFragmentView view;
     private UserSessionManager userSessionManager;
+
+    @Inject
+    public AuthPresenterImpl()
+    {}
 
     @Override
     public void init(AuthFragmentView view)
@@ -24,7 +39,35 @@ public class AuthPresenterImpl implements AuthPresenter
     @Override
     public void saveUserData(GoogleSignInAccount account, Context context)
     {
+        AuthService authService = new AuthService();
+
         userSessionManager = new UserSessionManager(context);
+
         userSessionManager.saveUserData(new User(account));
+
+        if(!StringUtils.isEmpty(account.getIdToken()))
+        {
+            authService.validateGoogleToken(account.getIdToken(), new RequestListener<ResponseBody>()
+            {
+                @Override
+                public void onSuccess(Call<ResponseBody> call, Response<ResponseBody> response)
+                {
+                    Log.d("Auth","success");
+                }
+
+                @Override
+                public void onErrorResponse(Call<ResponseBody> call, Response<ResponseBody> response)
+                {
+                    Log.d("Auth","error response");
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t)
+                {
+                    Log.d("Auth","fail");
+                }
+            });
+        }
     }
+
 }
---
+++ app/src/main/java/com/app/eventsapp/modules/auth/rest/AuthService.java
@@ -0,0 +1,36 @@
+package com.app.eventsapp.modules.auth.rest;
+
+import com.app.eventsapp.rest.base.RestService;
+import com.app.eventsapp.rest.request.RequestCallback;
+import com.app.eventsapp.rest.request.RequestListener;
+
+import javax.inject.Inject;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+
+
+/**
+ * Created by Grigory Kalyashov on 19.02.2017.
+ *
+ * Сервис аутентификации
+ */
+public class AuthService extends RestService
+{
+    @Inject
+    public AuthService()
+    {}
+
+    /**
+     * Отправить полученный google токен на валидацию
+     *
+     * @param googleToken google токен
+     * @param requestListener слушатель
+     */
+    public void validateGoogleToken(String googleToken, RequestListener<ResponseBody> requestListener)
+    {
+        AuthAPI authAPI = buildRetrofit().create(AuthAPI.class);
+        Call<ResponseBody> call = authAPI.validateGoogleToken(googleToken);
+        call.enqueue(new RequestCallback<>(requestListener));
+    }
+}
