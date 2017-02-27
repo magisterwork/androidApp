8081c91c72e98f24ff6792be11970163bcf863b9
kalyashov <kalyashov@yandex.ru>
2017-02-27 22:44:24 +0300

    Добавил сущности для запроса/ответа

---
+++ app/src/main/java/com/app/eventsapp/modules/auth/rest/response/IsFavoriteResponse.java
@@ -0,0 +1,49 @@
+package com.app.eventsapp.modules.auth.rest.response;
+
+/**
+ * Created by Grigory Kalyashov on 27.02.2017.
+ */
+
+public class IsFavoriteResponse
+{
+    private String status;
+    private String token;
+    private boolean favorite;
+
+    public IsFavoriteResponse(String status, String token, boolean favorite)
+    {
+        this.status = status;
+        this.token = token;
+        this.favorite = favorite;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public String getToken()
+    {
+        return token;
+    }
+
+    public void setToken(String token)
+    {
+        this.token = token;
+    }
+
+    public boolean isFavorite()
+    {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite)
+    {
+        this.favorite = favorite;
+    }
+}
