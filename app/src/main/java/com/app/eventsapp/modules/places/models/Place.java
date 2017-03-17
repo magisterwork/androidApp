1cf624cf380b4102cd9d8c7cd2cd36647222705d
kalyashov <kalyashov@yandex.ru>
2017-03-17 23:20:40 +0300

    Доработал сущость "Заведение"

--- app/src/main/java/com/app/eventsapp/modules/places/models/Place.java
+++ app/src/main/java/com/app/eventsapp/modules/places/models/Place.java
@@ -1,5 +1,9 @@
 package com.app.eventsapp.modules.places.models;
 
+import com.app.eventsapp.entities.PlaceCategory;
+
+import java.util.List;
+
 /**
  * Created by Grigory Kalyashov on 05.03.2017.
  *
@@ -11,19 +15,13 @@ public class Place
     private String name;
     private String imageUrl;
     private String address;
-    private String type;
+    private List<PlaceCategory> categories;
+    private double googleRating;
+    private double latitude;
+    private double longitude;
 
     public Place()
     {}
-
-    public Place(Long id, String name, String imageUrl, String address, String type)
-    {
-        this.id = id;
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.address = address;
-        this.type = type;
-    }
 
     public Long getId()
     {
@@ -65,13 +63,43 @@ public class Place
         this.address = address;
     }
 
-    public String getType()
+    public List<PlaceCategory> getCategories()
     {
-        return type;
+        return categories;
     }
 
-    public void setType(String type)
+    public void setCategories(List<PlaceCategory> categories)
     {
-        this.type = type;
+        this.categories = categories;
+    }
+
+    public double getGoogleRating()
+    {
+        return googleRating;
+    }
+
+    public void setGoogleRating(double googleRating)
+    {
+        this.googleRating = googleRating;
+    }
+
+    public double getLatitude()
+    {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude)
+    {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude()
+    {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude)
+    {
+        this.longitude = longitude;
     }
 }
